85f655fef9d34824a03f862e44537b79f6ce500b
Tạ Huỳnh Phúc <102749112+TaHuynhPhuc@users.noreply.github.com>
2024-06-04 17:45:11 +0700

    map2

--- Marimo Adventure/Assets/Phong/Scripts/MovingBlock.cs
+++ Marimo Adventure/Assets/Phong/Scripts/MovingBlock.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MovingBlock : MonoBehaviour
 {
     public float speed = 2.0f; // Tốc độ di chuyển của block
-    public float topY = 5.0f;  // Tọa độ Y của điểm trên cùng
-    public float bottomY = -5.0f; // Tọa độ Y của điểm dưới cùng
+    public float xPoint = 5.0f;  // Tọa độ Y của điểm trên cùng
+    public float yPoint = -5.0f; // Tọa độ Y của điểm dưới cùng
+    public bool verticalMode;
 
     private Vector3 direction = Vector3.up; // Hướng di chuyển ban đầu
 
@@ -15,15 +14,29 @@ public class MovingBlock : MonoBehaviour
         // Di chuyển block theo hướng hiện tại với tốc độ cố định
         transform.Translate(direction * speed * Time.deltaTime);
 
-        // Kiểm tra nếu block vượt quá điểm trên cùng
-        if (transform.position.y >= topY)
+        if (verticalMode)
         {
-            direction = Vector3.down; // Đổi hướng di chuyển xuống
+            // Kiểm tra nếu block vượt quá điểm trên cùng
+            if (transform.position.y >= xPoint)
+            {
+                direction = Vector3.down; // Đổi hướng di chuyển xuống
+            }
+            // Kiểm tra nếu block vượt quá điểm dưới cùng
+            else if (transform.position.y <= yPoint)
+            {
+                direction = Vector3.up; // Đổi hướng di chuyển lên
+            }
         }
-        // Kiểm tra nếu block vượt quá điểm dưới cùng
-        else if (transform.position.y <= bottomY)
+        else
         {
-            direction = Vector3.up; // Đổi hướng di chuyển lên
+            if (transform.position.x >= xPoint)
+            {
+                direction = Vector3.left;
+            }
+            else if (transform.position.x <= yPoint)
+            {
+                direction = Vector3.right;
+            }
         }
     }
 }
