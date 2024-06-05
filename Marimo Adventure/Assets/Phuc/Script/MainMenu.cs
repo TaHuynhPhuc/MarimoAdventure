54940d45f245edacd74e70651303045de2fdfeab
Peter <manhphong02@gmail.com>
2024-06-05 15:15:53 +0700

    xoá bớt file không liên quan

--- Marimo Adventure/Assets/Phuc/Script/MainMenu.cs
+++ Marimo Adventure/Assets/Phuc/Script/MainMenu.cs
@@ -5,15 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void playGame()
     {
         SceneManager.LoadSceneAsync(1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
