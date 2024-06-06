b860f9aaa57814b2bb6bbf9ab601182a51159c91
Tạ Huỳnh Phúc <102749112+TaHuynhPhuc@users.noreply.github.com>
2024-06-06 16:18:59 +0700

    fix

--- Marimo Adventure/Assets/Phuc/Script/MainMenu.cs
+++ Marimo Adventure/Assets/Phuc/Script/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void playGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadScene(1);
     }
     public void quitGame()
     {
